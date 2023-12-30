b5147c3c6dafe218ad31a97f17f24fa1fc273798
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2023-12-30 12:48:57 -0700

    add reilience to libc/memcpy

--- lib/libc/string/memcpy.c
+++ lib/libc/string/memcpy.c
@@ -3,6 +3,12 @@
 void *
 memcpy (void *restrict dst, const void *restrict src, size_t n)
 {
+#ifdef RESILIENT
+    if (!dst || !src)
+    {
+        return dst;
+    }
+#endif
     void *r = dst;
     while (n--)
         *(unsigned char *)dst++ = *(unsigned char *)src++;
